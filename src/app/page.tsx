a4c6efcb2e32146ba92a981a735542066d864120
Pradeep yadav <pradeep20020102@gmail.com>
2025-07-14 23:57:44 +0545

    all components are added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,14 @@ import { GithubSkeleton } from "@/components/skeletons/github-skeleton";
 import Image from "next/image";
 import ProjectList from '@/components/Project/project-list';
 import projects from '@/components/Project/project-data';
-import Fading from '@/components/fading';
+import { ScrollSVGPath } from '@/components/fading';
+import PageBackground from '@/components/background';
+import Skills from '@/components/skillSection';
+import Hero from '@/components/hero';
+import { typo } from '@/components/ui/typography';
+import ContactUs from '@/components/contactUs';
 const BLUR_FADE_DELAY = 0.04;
-
+import Footer from '@/components/footer';
 
 const GithubContributions = dynamic(() => import("@/components/github-contributions").then(mod => mod.GithubContributions), {
   ssr: true,
@@ -16,20 +21,29 @@ const GithubContributions = dynamic(() => import("@/components/github-contributi
 
 export default function Home() {
   return (
-     <div className="relative z-10 max-w-4xl mx-auto py-12 sm:py-24 px-6">
-     <main className="flex flex-col  space-y-10">
-      <Fading></Fading>
-       <section id="contributions">
+    <div className="relative z-10 max-w-4xl mx-auto py-12 sm:py-24 px-6 h-[2600px]">
+   
+        <main className="flex flex-col  space-y-10">
           <BlurFade delay={BLUR_FADE_DELAY * 10}>
-            <h2 className="text-xl font-bold mb-2">GitHub Contributions</h2>
-            <GithubContributions />
+            <Hero />
+            <Skills/>
           </BlurFade>
-        </section>
-         <BlurFade delay={BLUR_FADE_DELAY * 10}>
-            <h2 className="text-xl font-bold mb-2">GitHub Contributions</h2>
+          <BlurFade delay={BLUR_FADE_DELAY * 10}>
+            {/* <h2 className="text-xl font-bold mb-2">GitHub Contributions</h2> */}
             <ProjectList projects={projects.slice(0, 4)} metadata />
           </BlurFade>
-    </main>
+          <section id="contributions" className='py-10'>
+            <BlurFade delay={BLUR_FADE_DELAY * 10}>
+              <div className="my-4 space-y-8 bg-background">
+              <h2 className={typo({ variant: "h2" })}>GitHub Contributions</h2>
+              <GithubContributions />
+
+              </div>
+            </BlurFade>
+          </section>
+          <ContactUs></ContactUs>
+          <Footer/>
+        </main>
     </div>
   );
 }
