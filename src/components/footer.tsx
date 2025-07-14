8f59e1dbe3d735ccabb27629f40f129321afbe6c
Pradeep yadav <pradeep20020102@gmail.com>
2025-07-15 00:59:09 +0545

    heart emoji added

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="!mt-auto flex flex-col items-center justify-center py-4">
       <div className="flex w-full flex-wrap items-center justify-center gap-2 text-center sm:justify-between">
-        <span>&copy; {new Date().getFullYear()} Developed with 🩵 by Pradeep</span>
+        <span>&copy; {new Date().getFullYear()} Developed with ❤️ by Pradeep</span>
       </div>
     </footer>
   );
