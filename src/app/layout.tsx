972179c5bd77382d4a7f14f5e4b0e3c9cca48d9d
Pradeep yadav <pradeep20020102@gmail.com>
2025-07-14 23:57:30 +0545

    bg grid created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/Theme-provider";
+import PageBackground from "@/components/background";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,14 +28,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <PageBackground>
         <ThemeProvider
          attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
             >
-          {children}
+
+         {children}
         </ThemeProvider>
+                 </PageBackground>
       </body>
     </html>
   );
