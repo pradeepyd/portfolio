bdca3f989ad64fa369e06e82c2b6a43ba7d92a09
Pradeep yadav <pradeep20020102@gmail.com>
2025-07-14 23:58:49 +0545

    footer componet created

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="!mt-auto flex flex-col items-center justify-center py-4">
+      <div className="flex w-full flex-wrap items-center justify-center gap-2 text-center sm:justify-between">
+        <span>&copy; {new Date().getFullYear()} Developed with 🩵 by Pradeep</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
