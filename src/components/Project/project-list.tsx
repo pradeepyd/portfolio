4fc5f6d78fece9f62ad0728d4f32bd1a2ee6f751
Pradeep yadav <pradeep20020102@gmail.com>
2025-07-15 12:36:26 +0545

    padding and margin are made

--- src/components/Project/project-list.tsx
+++ src/components/Project/project-list.tsx
@@ -6,10 +6,10 @@ import ProjectItem from "./project-item";
 
 const ProjectList = ({ projects, metadata }: { projects: TProject[]; metadata?: boolean }) => {
   return (
-    <section aria-label="projects" className="mt-5 space-y-6" id="main-content">
+    <section aria-label="projects" className="mt-8 space-y-6" id="main-content">
       {metadata && <h2 className={typo({ variant: "h2" })}>Featured Projects</h2>}
 
-      <div className="!mt-8">
+      <div className="!mt-4">
         {projects.length > 0 ? (
           <ol className="grid gap-8 md:grid-cols-2" role="list">
             {projects.map((project) => (
